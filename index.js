18269ba5abc594d0bc6f56958b92aab4560a5579
assemblyWeb <alfio@assemblyweb.net>
2025-12-12 14:15:36 -0300

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 const arquero = {
     // prinmera vuelta
-  1: { disparo1: 'M', disparo2: 'M' },   // Estación 10 original
-  2: { disparo1: 'M', disparo2: 'M' },   // Estación 11
-  3: { disparo1: 'M', disparo2: 'M' },   // Estación 12
-  4: { disparo1: 5, disparo2: 'M' },   // Estación 13
-  5: { disparo1: 10, disparo2: 5 },   // Estación 14
-  6: { disparo1: 5, disparo2: 5 },   // Estación 15
-  7: { disparo1: 5, disparo2: 5 },   // Estación 16
-  8: { disparo1: 8, disparo2: 'M' },   // Estación 17
-  9: { disparo1: 8, disparo2: 5 },   // Estación 18
-  10: { disparo1: 5, disparo2: 5 },  // Estación 19
-  11: { disparo1: 'M', disparo2: 'M' },  // Estación 20
-  12: { disparo1: 8, disparo2: 8 },  // Estación 21
+  1: { disparo1: 'M', disparo2: 'M', estacion: 10 },   // Estación 10 original
+  2: { disparo1: 'M', disparo2: 'M', estacion: 11 },   // Estación 11
+  3: { disparo1: 'M', disparo2: 'M', estacion: 12 },   // Estación 12
+  4: { disparo1: 5, disparo2: 'M', estacion: 1 },   // Estación 13
+  5: { disparo1: 10, disparo2: 5, estacion: 2 },   // Estación 14
+  6: { disparo1: 5, disparo2: 5, estacion: 3 },   // Estación 15
+  7: { disparo1: 5, disparo2: 5, estacion: 4 },   // Estación 16
+  8: { disparo1: 8, disparo2: 'M', estacion: 5 },   // Estación 17
+  9: { disparo1: 8, disparo2: 5, estacion: 6 },   // Estación 18
+  10: { disparo1: 5, disparo2: 5, estacion: 7 },  // Estación 19
+  11: { disparo1: 'M', disparo2: 'M', estacion: 8 },  // Estación 20
+  12: { disparo1: 8, disparo2: 8, estacion: 9 },  // Estación 21
   //segunda vuelta
-  13: { disparo1: 5, disparo2: 5 },  // Estación 22
-  14: { disparo1: 5, disparo2: 5 },  // Estación 23
-  15: { disparo1: 8, disparo2: 0 },    // Estación 24
-  16: { disparo1: 5, disparo2: 5 },  // Estación 1 original
-  17: { disparo1: 8, disparo2: 8 },  // Estación 2
-  18: { disparo1: 5, disparo2: 'M' },  // Estación 3
-  19: { disparo1: 'M', disparo2: 'M' },  // Estación 4
-  20: { disparo1: 5, disparo2: 5 },  // Estación 5
-  21: { disparo1: 5, disparo2: 'M' },  // Estación 6
-  22: { disparo1: 8, disparo2: 5 },  // Estación 7
-  23: { disparo1: 'M', disparo2: 'M' },  // Estación 8
-  24: { disparo1: 10, disparo2: 5 }    // Estación 9
+  13: { disparo1: 5, disparo2: 5, estacion: 10 },  // Estación 22
+  14: { disparo1: 5, disparo2: 5, estacion: 11 },  // Estación 23
+  15: { disparo1: 8, disparo2: 0, estacion: 12 },    // Estación 24
+  16: { disparo1: 5, disparo2: 5, estacion: 1 },  // Estación 1 original
+  17: { disparo1: 8, disparo2: 8, estacion: 2 },  // Estación 2
+  18: { disparo1: 5, disparo2: 'M', estacion: 3 },  // Estación 3
+  19: { disparo1: 'M', disparo2: 'M', estacion: 4 },  // Estación 4
+  20: { disparo1: 5, disparo2: 5, estacion: 5 },  // Estación 5
+  21: { disparo1: 5, disparo2: 'M', estacion: 6 },  // Estación 6
+  22: { disparo1: 8, disparo2: 5, estacion: 7 },  // Estación 7
+  23: { disparo1: 'M', disparo2: 'M', estacion: 8 },  // Estación 8
+  24: { disparo1: 10, disparo2: 5, estacion: 9 }    // Estación 9
 };
 const estaciones = {
-  1: { distancia: 17, altura: 'bajo' },
-  2: { distancia: 18, altura: 'alto' },
-  3: { distancia: 12.2, altura: 'alto' },
-  4: { distancia: 19.2, altura: 'medio' },
-  5: { distancia: 19.7, altura: 'medio' },
-  6: { distancia: 26, altura: 'medio' },
-  7: { distancia: 13.5, altura: 'bajo' },
-  8: { distancia: 22.4, altura: 'bajo' },
-  9: { distancia: 16, altura: 'bajo' },
-  10: { distancia: 13, altura: 'bajo' },
-  11: { distancia: 11.5, altura: 'bajo' },
-  12: { distancia: 19.2, altura: 'bajo' }
+  1: { distancia: 17, altura: 'bajo', animal: {tipo: 'zorro', superficie: "media"}},
+  2: { distancia: 18, altura: 'alto', animal: {tipo: 'lobo', superficie: "grande"} },
+  3: { distancia: 12.2, altura: 'alto', animal: {tipo: 'caracol', superficie: "chica"} },
+  4: { distancia: 19.2, altura: 'medio', animal: {tipo: 'mandril', superficie: "media"} },
+  5: { distancia: 19.7, altura: 'medio', animal: {tipo: 'mulita', superficie: "chica"} },
+  6: { distancia: 26, altura: 'medio', animal: {tipo: 'oso', superficie: "grande"} },
+  7: { distancia: 13.5, altura: 'bajo', animal: {tipo: 'ardilla', superficie: "chica"} },
+  8: { distancia: 22.4, altura: 'bajo', animal: {tipo: 'buho', superficie: "media"} },
+  9: { distancia: 16, altura: 'bajo', animal: {tipo: 'liebre', superficie: "chica"} },
+  10: { distancia: 13, altura: 'bajo', animal: {tipo: 'perdiz', superficie: "chica"} },
+  11: { distancia: 11.5, altura: 'bajo', animal: {tipo: 'gato', superficie: "media"} },
+  12: { distancia: 19.2, altura: 'bajo', animal: {tipo: 'condor', superficie: "media"} }
 };
 
 
