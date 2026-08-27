2f7594baef113147b058ec62875f59c9c758fd79
assemblyWeb <alfio@assemblyweb.net>
2026-08-27 18:04:28 -0300

    fix: correct data handling and improve chart rendering for better performance

---
+++ index.js
@@ -0,0 +1,42 @@
+const arquero = {
+  1: { disparo1: 5, disparo2: 5 },   // Estación 10 original
+  2: { disparo1: 'M', disparo2: 'M' },   // Estación 11
+  3: { disparo1: 'M', disparo2: 'M' },   // Estación 12
+  4: { disparo1: 5, disparo2: 'M' },   // Estación 13
+  5: { disparo1: 10, disparo2: 5 },   // Estación 14
+  6: { disparo1: 5, disparo2: 5 },   // Estación 15
+  7: { disparo1: 5, disparo2: 5 },   // Estación 16
+  8: { disparo1: 8, disparo2: 'M' },   // Estación 17
+  9: { disparo1: 8, disparo2: 5 },   // Estación 18
+  10: { disparo1: 5, disparo2: 5 },  // Estación 19
+  11: { disparo1: 'M', disparo2: 'M' },  // Estación 20
+  12: { disparo1: 8, disparo2: 8 },  // Estación 21
+  13: { disparo1: 5, disparo2: 5 },  // Estación 22
+  14: { disparo1: 5, disparo2: 5 },  // Estación 23
+  15: { disparo1: 8, disparo2: 0 },    // Estación 24
+
+  // Ahora agregamos las estaciones que faltan del 1 al 9
+  16: { disparo1: 5, disparo2: 5 },  // Estación 1 original
+  17: { disparo1: 8, disparo2: 8 },  // Estación 2
+  18: { disparo1: 5, disparo2: 'M' },  // Estación 3
+  19: { disparo1: 'M', disparo2: 'M' },  // Estación 4
+  20: { disparo1: 5, disparo2: 'M' },  // Estación 5
+  21: { disparo1: 10, disparo2: 5 },  // Estación 6
+  22: { disparo1: 5, disparo2: 5 },  // Estación 7
+  23: { disparo1: 8, disparo2: 'M' },  // Estación 8
+  24: { disparo1: 8, disparo2: 5 }    // Estación 9
+};
+const estaciones = {
+  1: { distancia: 17, altura: 'bajo' },
+  2: { distancia: 18, altura: 'alto' },
+  3: { distancia: 12.2, altura: 'alto' },
+  4: { distancia: 19.2, altura: 'medio' },
+  5: { distancia: 19.7, altura: 'medio' },
+  6: { distancia: 26, altura: 'medio' },
+  7: { distancia: 13.5, altura: 'bajo' },
+  8: { distancia: 22.4, altura: 'bajo' },
+  9: { distancia: 16, altura: 'bajo' },
+  10: { distancia: 13, altura: 'bajo' },
+  11: { distancia: 11.5, altura: 'bajo' },
+  12: { distancia: 19.2, altura: 'bajo' }
+};
